6171a0913dda02c09cd043c15535250e29854cbe
pitagoras <sanron27@hotmail.com>
2023-12-30 19:28:13 -0500

    initial commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  template: `
+    <header class="audiowide-font">
+      <!-- Contenido del encabezado -->
+    </header>
+  `,
+})
+export class HeaderComponent {
+
+}
